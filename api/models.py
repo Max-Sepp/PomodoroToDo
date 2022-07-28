b9a79b820b7e54956a01a900ae4d3411d7ac1fa3
Max Smith <>
2022-07-28 12:01:02 +0100

    added todo model to store ToDo tasks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+import random
+import string
+
+
+def generate_code():
+    while True:
+        letters = string.ascii_letters + string.digits
+        code = "".join(random.choice(letters) for i in range(10))
+        if Blog_Post_Storage.objects.filter(code=code).count() == 0:
+            break
+    return code
+
+
+class Todo(models.Model):
+    id = models.CharField(
+        max_length=11, default=generate_code, unique=True, primary_key=True
+    )
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=250, default="")
+    creator = models.CharField(max_length=15)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
