f8a2a2cc7696837734c003dcb90e6232db1cec32
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2022-07-31 21:40:29 +0100

    reorganised to make better structured

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,28 +1,12 @@
-import React, { useState } from 'react';
-import Timer from './Timer';
-import SettingsContext from './SettingsContext';
-import Settings from './Settings';
+import React from 'react';
+import PomodoroTimer from './PomodoroTimer/PomodoroTimer'
 
 function HomePage() {
-
-    const [settings, setSettings] = useState(false);
-    const [workTime, setWorkTime] = useState(25);
-    const [breakTime, setBreakTime] = useState(5);
     return (
-        <SettingsContext.Provider value={{
-            settings,
-            setSettings,
-            workTime,
-            setWorkTime,
-            breakTime,
-            setBreakTime
-        }}>
-            <div className='grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto'>
-                <div className='md:col-span-2'>{settings ? <Timer /> : <Settings />}</div>
-
-                <div className=''>World!</div>
-            </div>
-        </SettingsContext.Provider >
+        <div className='max-w-7xl grid md:grid-cols-3 grid-cols-1'>
+            <div className='md:col-span-2'><PomodoroTimer /></div>
+            <div>Todo</div>
+        </div>
     )
 }
 
---
+++ frontend/src/components/HomePage/PomodoroTimer/PomodoroTimer.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Timer from './Timer';
+import SettingsContext from './SettingsContext';
+import Settings from './Settings';
+
+function HomePage() {
+
+    const [settings, setSettings] = useState(false);
+    const [workTime, setWorkTime] = useState(25);
+    const [breakTime, setBreakTime] = useState(5);
+    return (
+        <SettingsContext.Provider value={{
+            settings,
+            setSettings,
+            workTime,
+            setWorkTime,
+            breakTime,
+            setBreakTime
+        }}>
+            {settings ? <Timer /> : <Settings />}
+        </SettingsContext.Provider >
+    )
+}
+
+export default HomePage
