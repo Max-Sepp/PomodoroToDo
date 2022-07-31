5eedb176e0af624029337c4b18b92ed47b17e490
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2022-07-31 21:21:24 +0100

    completed pomodoro timer

--- frontend/src/components/HomePage/Settings.jsx
+++ frontend/src/components/HomePage/Settings.jsx
@@ -11,10 +11,15 @@ function Settings() {
 
     return (
         <div className='mx-auto w-fit pt-5'>
+            <h3 className='text-indigo font-bold md:text-4xl text-3xl '>Work Timer: {settings.workTime}</h3>
             <Slider
                 className='my-5'
                 min={1}
                 max={120}
+                value={settings.workTime}
+                onChange={(value) => {
+                    settings.setWorkTime(value)
+                }}
                 railStyle={{
                     height: 2
                 }}
@@ -22,7 +27,30 @@ function Settings() {
                     height: 28,
                     width: 28,
                     marginTop: -14,
-                    backgroundColor: "red",
+                    backgroundColor: "#FB5012",
+                    border: 0
+                }}
+                trackStyle={{
+                    background: "none"
+                }}
+            />
+            <h3 className='text-indigo font-bold md:text-4xl text-3xl '>Break Timer: {settings.breakTime}</h3>
+            <Slider
+                className='my-5'
+                min={1}
+                max={30}
+                value={settings.breakTime}
+                onChange={(value) => {
+                    settings.setBreakTime(value)
+                }}
+                railStyle={{
+                    height: 2
+                }}
+                handleStyle={{
+                    height: 28,
+                    width: 28,
+                    marginTop: -14,
+                    backgroundColor: "#FB5012",
                     border: 0
                 }}
                 trackStyle={{
